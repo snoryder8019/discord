d139ad8a5a7fb987ca320c4bf368ce32edb5b2ba
snoryder8019 <snoryder8019@gmail.com>
2022-10-03 09:13:33 -0600

    added herobot and quotes

--- bot.js
+++ bot.js
@@ -11,7 +11,29 @@ const timQuotes= [
 "tim's birthday is in April, he was born on the same day as Saddam Hussen",
 "if tim falls in the woods, does he make an insult towards your rocket laegue play?",
 "Pogonophobia is the fear of beards, that's funny because charlie blackmon hits bombs on tim",
-"wait.....did you hear that? tim just got a bit older"
+"wait.....did you hear that? tim just got a bit older",
+"this isnt tim hate....its tim love, I am the tim bot",
+"give tim 50 feet",
+"he's an enchanter, This is the tim bot",
+"timmmmmmayyyyyyyy!!!!!!",
+"tim bot is the future. I am the UNIVERSE!!!!!"
+
+]
+const heroQuotes= [
+"psssst, hail Hydra",
+"hero bot is the future. I am the UNIVERSE!!!!!",
+"You Either Die A Hero Or Live Long Enough To See Yourself Become The Villain.",
+"Don't Be What They Made You.",
+"If You're Nothing Without The Suit, Then You Shouldn't Have It.",,
+"Just Because Someone Stumbles And Loses Their Path, Doesn't Mean They're Lost Forever.",
+"It's Not Who I Am Underneath, But What I Do That Defines Me.",
+"and I.....am......Hero Bot",
+"I Can Do This All Day."
+
+
+
+
+
 
 ]
 client.on('ready', () =>{
@@ -19,8 +41,8 @@ console.log('scotts bot is working')
 })
 client.on('message', async (message)=>{
  let msg = message.content
-  if(msg.includes('tim')===true && message.author.bot === false){
-       await message.channel.send(timQuotes[random(0,timQuotes.length)])
+  if(msg.includes('hero')===true && message.author.bot === false){
+       await message.channel.send(heroQuotes[random(0,heroQuotes.length)])
 
   }
   
